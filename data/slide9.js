75fa7054e2e2008021d5da9e5a0aa472ae8cc115
temha-42 <iscanahmett@gmail.com>
2023-04-28 11:09:47 +0300

    etkileşim

---
+++ data/slide9.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{E1E3D5DA-B6F3-4B16-B00C-8FF0E79D0783}'];loadHandler&&loadHandler(8, '<div id="spr0_2e2858"><div id="spr1_2e2858" class="kern slide"><img id="img0_2e2858" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_2e2858" class="kern slide"><div id="wo_8_0"></div></div></div>', '{"s":[]}');})();
